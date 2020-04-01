1dee7d88f9b7ce2fa01c84c16ace512198db2e51
AhmedSa-mir <asamir97@hotmail.com>
2020-04-01 12:12:46 +0200

    Solved openresty deffault location issue

--- package/frontend_src_sapper/build_frontend.sh
+++ package/frontend_src_sapper/build_frontend.sh
@@ -1,8 +1,4 @@
 
 npm run export
-cp __sapper__/export/* ../frontend/ -R
-
-DEFAULT_DIR="/sandbox/cfg/nginx/default_openresty_threebot/html/"
-[ ! -d "$DEFAULT_DIR" ] && mkdir $DEFAULT_DIR
-
-cp __sapper__/export/* $DEFAULT_DIR -R
+rm -r ../frontend/*
+cp __sapper__/export/zerobot/todo/* ../frontend/ -R
